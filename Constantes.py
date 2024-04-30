cd29c6ee7537382ff3410f6178c7f684e9944045
Lívia Carvalho <liviagc@al.insper.edu.br>
2024-04-30 20:43:31 -0300

    Arquivo: constantes

---
+++ Constantes.py
@@ -0,0 +1,60 @@
+CONFIGURACAO = {
+    'destroyer': 3,
+    'porta-avioes': 5,
+    'submarino': 2,
+    'torpedeiro': 3,
+    'cruzador': 2,
+    'couracado': 4
+}
+
+PAISES =  {
+    'Brasil': {
+        'cruzador': 1,
+        'torpedeiro': 2,
+        'destroyer': 1,
+        'couracado': 1,
+        'porta-avioes': 1
+    }, 
+    'França': {
+        'cruzador': 3, 
+        'porta-avioes': 1, 
+        'destroyer': 1, 
+        'submarino': 1, 
+        'couracado': 1
+    },
+    'Austrália': {
+        'couracado': 1,
+        'cruzador': 3, 
+        'submarino': 1,
+        'porta-avioes': 1, 
+        'torpedeiro': 1
+    },
+    'Rússia': {
+        'cruzador': 1,
+        'porta-avioes': 1,
+        'couracado': 2,
+        'destroyer': 1,
+        'submarino': 1
+    },
+    'Japão': {
+        'torpedeiro': 2,
+        'cruzador': 1,
+        'destroyer': 2,
+        'couracado': 1,
+        'submarino': 1
+    }
+}
+
+ALFABETO = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+CORES = {
+    'reset': '\u001b[0m',
+    'red': '\u001b[31m',
+    'black': '\u001b[30m',
+    'green': '\u001b[32m',
+    'yellow': '\u001b[33m',
+    'blue': '\u001b[34m',
+    'magenta': '\u001b[35m',
+    'cyan': '\u001b[36m',
+    'white': '\u001b[37m'
+}
