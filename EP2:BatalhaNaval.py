92408c7c6d1b457a00228c86fd7506e2cb433159
Lívia Carvalho <liviagc@al.insper.edu.br>
2024-05-02 00:27:54 -0300

    ajuste para impressão dos (dois) mapas + adaptacao das funções seguintes + concerto de erro para quando um valor inválido é inserido

--- EP2:BatalhaNaval.py
+++ EP2:BatalhaNaval.py
@@ -64,6 +64,14 @@ CORES = {
     'white': '\u001b[37m'
 }
 
+PROXIMOS = {
+    'Brasil': {'cruzador': 2, 'destroyer': 1, 'porta-avioes': 1, 'submarino': 2, 'torpedeiro': 1},
+    'França': {'cruzador': 2, 'destroyer': 1, 'porta-avioes': 1, 'submarino': 2, 'torpedeiro': 1},
+    'Austrália': {'cruzador': 2, 'destroyer': 1, 'porta-avioes': 1, 'submarino': 2, 'torpedeiro': 1},
+    'Rússia': {'cruzador': 2, 'destroyer': 1, 'porta-avioes': 1, 'submarino': 2, 'torpedeiro': 1},
+    'Japão': {'cruzador': 2, 'destroyer': 1, 'porta-avioes': 1, 'submarino': 2, 'torpedeiro': 1}
+}
+
 ##############################################################################################################################################
 #   FUNÇÕES OBRIGATÓRIAS  #
 
@@ -128,58 +136,80 @@ def foi_derrotado(matriz):
 ##############################################################################################################################################
 #   OUTRAS FUNÇÕES  #
 
-    # Função para imprimir o mapa
-def imprime_mapa(mapa):
-    N = len(mapa)
+ # Função para imprimir o mapa
+def imprime_mapa(mapa_jogador, mapa_computador, nome_pais_jogador, nome_pais_computador):
+    N = len(mapa_jogador)
+    print(f"    JOGADOR - {nome_pais_jogador:20}{'COMPUTADOR - ' + nome_pais_computador:20}")
+    print("   " + "  ".join([ALFABETO[i] for i in range(N)]), end="")
+    print("       ", end="")
     print("   " + "  ".join([ALFABETO[i] for i in range(N)]))
     for i in range(N):
         print(f"{i+1:2} ", end="")
         for j in range(N):
-            if mapa[i][j] == ' ':
-                print(CORES['cyan'] + mapa[i][j] + CORES['reset'], end="  ")
-            elif mapa[i][j] == 'X':
-                print(CORES['red'] + mapa[i][j] + CORES['reset'], end="  ")
+            if mapa_jogador[i][j] == ' ':
+                print(CORES['cyan'] + mapa_jogador[i][j] + CORES['reset'], end="  ")
+            elif mapa_jogador[i][j] == 'X':
+                print(CORES['red'] + mapa_jogador[i][j] + CORES['reset'], end="  ")
             else:
-                print(CORES['yellow'] + mapa[i][j] + CORES['reset'], end="  ")
+                print(CORES['yellow'] + mapa_jogador[i][j] + CORES['reset'], end="  ")
+        print(f" {i+1}", end="")
+        print("      ", end="")
+        print(f"{i+1:2} ", end="")
+        for j in range(N):
+            if mapa_computador[i][j] == ' ' or mapa_computador[i][j] == 'N':
+                print(CORES['cyan'] + ' ' + CORES['reset'], end="  ")
+            else:
+                print(CORES['cyan'] + mapa_computador[i][j] + CORES['reset'], end="  ")
         print(f" {i+1}")
+    print("   " + "  ".join([ALFABETO[i] for i in range(N)]), end="")
+    print("       ", end="")
     print("   " + "  ".join([ALFABETO[i] for i in range(N)]))
 
-    # Função para o jogador real alocar seus navios
-def aloca_navios_humano(mapa, nome_pais):
-    for tipo_navio, quantidade in PAISES[nome_pais].items():
-        print(f"Aloque os navios do tipo {tipo_navio}:")
+# Função para o jogador real alocar seus navios
+def aloca_navios_humano(mapa_jogador, mapa_computador, nome_pais_jogador, nome_pais_computador):
+    print(f"Você escolheu a nação {nome_pais_jogador}")
+    print("Agora é a sua vez de alocar seus navios de guerra!")
+    imprime_mapa(mapa_jogador, mapa_computador, nome_pais_jogador, nome_pais_computador)
+    for tipo_navio, quantidade in PAISES[nome_pais_jogador].items():
+        print(f"\nAloque os navios do tipo {tipo_navio}:")
+        print("  ", end="")
+        for proximo_tipo, quantidade in PROXIMOS[nome_pais_jogador].items():
+            print(proximo_tipo, end=", ")
+        print()
+        tipo_navio_bloco = CONFIGURACAO[tipo_navio]
         for i in range(quantidade):
             sucesso = False
             while not sucesso:
-                imprime_mapa(mapa)
-                print(f"\nAlocação do {i+1}º {tipo_navio}")
-                linha = input("Digite o número da linha (1 a 10): ")
+                print(f"\nAlocação do {i+1}º {tipo_navio}\n")
+                imprime_mapa(mapa_jogador, mapa_computador, nome_pais_jogador, nome_pais_computador)
+                linha = input("\nDigite o número da linha (1 a 10): ")
                 if not linha.isdigit() or int(linha) < 1 or int(linha) > 10:
                     print("Linha inválida, tente novamente.")
-                else:
-                    linha = int(linha) - 1
-                    coluna = input("Digite a letra da coluna (A a J): ").upper()
-                    if coluna not in ALFABETO:
-                        print("Coluna inválida, tente novamente.")
+                    continue
+                linha = int(linha) - 1
+                coluna = input("Digite a letra da coluna (A a J): ").upper()
+                if coluna not in ALFABETO:
+                    print("Coluna inválida, tente novamente.")
+                    continue
+                coluna = ALFABETO.index(coluna)
+                orientacao = input("Digite a orientação do navio (v para vertical, h para horizontal): ").lower()
+                if posicao_suporta(mapa_jogador, tipo_navio_bloco, linha, coluna, orientacao):
+                    if orientacao == 'v':
+                        for j in range(linha, linha + tipo_navio_bloco):
+                            mapa_jogador[j][coluna] = 'N'
                     else:
-                        coluna = ALFABETO.index(coluna)
-                        orientacao = input("Digite a orientação do navio (v para vertical, h para horizontal): ").lower()
-                        if posicao_suporta(mapa, CONFIGURACAO[tipo_navio], linha, coluna, orientacao):
-                            if orientacao == 'v':
-                                for j in range(linha, linha + CONFIGURACAO[tipo_navio]):
-                                    mapa[j][coluna] = 'N'
-                            else:
-                                for j in range(coluna, coluna + CONFIGURACAO[tipo_navio]):
-                                    mapa[linha][j] = 'N'
-                            sucesso = True
-                        else:
-                            print("Posição inválida, tente novamente.")
+                        for j in range(coluna, coluna + tipo_navio_bloco):
+                            mapa_jogador[linha][j] = 'N'
+                    sucesso = True
+                else:
+                    print("Posição inválida, tente novamente.")
+        print()  
 
-    # Função para o jogador real realizar um ataque
-def ataque_humano(mapa):
+# Função para o jogador real realizar um ataque
+def ataque_humano(mapa_jogador, mapa_computador, nome_pais_jogador, nome_pais_computador):
     sucesso = False
     while not sucesso:
-        imprime_mapa(mapa)
+        imprime_mapa(mapa_jogador, mapa_computador, nome_pais_jogador, nome_pais_computador)
         print("\nAtaque:")
         linha = input("Digite o número da linha (1 a 10): ")
         if not linha.isdigit() or int(linha) < 1 or int(linha) > 10:
@@ -191,32 +221,35 @@ def ataque_humano(mapa):
                 print("Coluna inválida, tente novamente.")
             else:
                 coluna = ALFABETO.index(coluna)
-                if mapa[linha][coluna] == ' ':
-                    print("Água!")
-                    mapa[linha][coluna] = 'O'
-                    sucesso = True
-                elif mapa[linha][coluna] == 'N':
-                    print("Acertou um navio!")
-                    mapa[linha][coluna] = 'X'
-                    sucesso = True
+                if coluna < 0 or coluna >= 10:
+                    print("Coluna inválida, tente novamente.")
                 else:
-                    print("Já atacou essa posição, tente novamente.")
+                    if mapa_computador[linha][coluna] == ' ':
+                        print("Água!")
+                        mapa_computador[linha][coluna] = 'O'
+                        sucesso = True
+                    elif mapa_computador[linha][coluna] == 'N':
+                        print("Acertou um navio!")
+                        mapa_computador[linha][coluna] = 'X'
+                        sucesso = True
+                    else:
+                        print("Já atacou essa posição, tente novamente.")
 
-    # Função para o jogador computador realizar um ataque
-def ataque_computador(mapa):
+# Função para o jogador computador realizar um ataque
+def ataque_computador(mapa_jogador, mapa_computador, nome_pais_jogador, nome_pais_computador):
     sucesso = False
     while not sucesso:
-        linha = random.randint(0, len(mapa)-1)
-        coluna = random.randint(0, len(mapa)-1)
-        if mapa[linha][coluna] == ' ':
+        linha = random.randint(0, len(mapa_jogador)-1)
+        coluna = random.randint(0, len(mapa_jogador)-1)
+        if mapa_jogador[linha][coluna] == ' ':
             print("O computador atacou: ", ALFABETO[coluna] + str(linha+1))
             print("Água!")
-            mapa[linha][coluna] = 'O'
+            mapa_jogador[linha][coluna] = 'O'
             sucesso = True
-        elif mapa[linha][coluna] == 'N':
+        elif mapa_jogador[linha][coluna] == 'N':
             print("O computador atacou: ", ALFABETO[coluna] + str(linha+1))
             print("Acertou um navio!")
-            mapa[linha][coluna] = 'X'
+            mapa_jogador[linha][coluna] = 'X'
             sucesso = True
 
 # Função para escolher aleatoriamente o país do computador
@@ -229,12 +262,13 @@ def imprime_paises():
     for i, (pais, navios) in enumerate(PAISES.items(), 1):
         print(f"{i}: {pais}")
         for tipo_navio, quantidade in navios.items():
-            print(f"{quantidade} {tipo_navio}")
-        print()
+            print(f"   {quantidade} {tipo_navio}")
+        print()  # Linha em branco adicionada
 
 ##############################################################################################################################################
 #   Colocando o jogo para funcionar!  #
 
+# Função principal do jogo
 def batalha_naval():
     print("\033[1;30;47m======================================\033[m")
     print("\033[1;30;47m|                                    |\033[m")
@@ -252,8 +286,8 @@ def batalha_naval():
         print("Escolha inválida, tente novamente.")
         escolha = int(input("Qual o número da nação da sua frota? "))
     nome_pais_jogador = list(PAISES.keys())[escolha - 1]
-    aloca_navios_humano(mapa_jogador, nome_pais_jogador)
-    aloca_navios(mapa_computador, [CONFIGURACAO[tipo] for tipo in PAISES[random.choice(list(PAISES.keys()))].keys()])
+    aloca_navios_humano(mapa_jogador, mapa_computador, nome_pais_jogador, nome_pais_computador)
+    aloca_navios(mapa_computador, [CONFIGURACAO[tipo] for tipo in PAISES[nome_pais_computador].keys()])
     jogador_venceu = False
     computador_venceu = False
     vez_jogador = random.choice([True, False])
@@ -264,18 +298,18 @@ def batalha_naval():
     while not jogador_venceu and not computador_venceu:
         if vez_jogador:
             print("\nSua vez de atacar!")
-            ataque_humano(mapa_computador)
+            ataque_humano(mapa_jogador, mapa_computador, nome_pais_jogador, nome_pais_computador)
             if foi_derrotado(mapa_computador):
                 jogador_venceu = True
         else:
             print("\nVez do computador atacar!")
-            ataque_computador(mapa_jogador)
+            ataque_computador(mapa_jogador, mapa_computador, nome_pais_jogador, nome_pais_computador)
             if foi_derrotado(mapa_jogador):
                 computador_venceu = True
         vez_jogador = not vez_jogador
     print("\nJogo encerrado!")
     if jogador_venceu:
-        print(f"Parabéns! Você venceu representando o país {nome_pais}!")
+        print(f"Parabéns! Você venceu representando o país {nome_pais_jogador}!")
     else:
         print("Você perdeu! O computador venceu.")
 
