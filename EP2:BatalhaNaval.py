cf1daeee7977f0f256befa7b00829c1014dfdb1e
Lívia Carvalho <liviagc@al.insper.edu.br>
2024-05-02 22:54:56 -0300

    mapa_computador

--- EP2:BatalhaNaval.py
+++ EP2:BatalhaNaval.py
@@ -89,43 +89,43 @@ def cria_mapa(N):
     return mapa
 
     # Navio pode ser alocado na posição?
-def posicao_suporta(mapa, blocos, linha, coluna, orientacao):
-    if linha < 0 or coluna < 0 or linha >= len(mapa) or coluna >= len(mapa):
+def posicao_suporta(mapa_computador, blocos, linha, coluna, orientacao):
+    if linha < 0 or coluna < 0 or linha >= len(mapa_computador) or coluna >= len(mapa_computador):
         return False
     if orientacao not in ['v', 'h']:
         return False
     if orientacao == 'v':
-        if linha + blocos > len(mapa):
+        if linha + blocos > len(mapa_computador):
             return False
         for i in range(linha, linha + blocos):
-            if mapa[i][coluna] != ' ':
+            if mapa_computador[i][coluna] != ' ':
                 return False
-    elif coluna + blocos > len(mapa):
+    elif coluna + blocos > len(mapa_computador):
         return False
     else:
         for j in range(coluna, coluna + blocos):
-            if mapa[linha][j] != ' ':
+            if mapa_computador[linha][j] != ' ':
                 return False
     return True
 
     # Alocando navios para o computador
-def aloca_navios(mapa, blocos):
-    n = len(mapa)
+def aloca_navios(mapa_computador, blocos):
+    n = len(mapa_computador)
     for navio in blocos:
         alocando_navio = False
         while not alocando_navio:
             linha = random.randint(0, n-1)
             coluna = random.randint(0, n-1)
             orientacao = random.choice(['h', 'v'])
-            if posicao_suporta(mapa, navio, linha, coluna, orientacao):
+            if posicao_suporta(mapa_computador, navio, linha, coluna, orientacao):
                 if orientacao == 'v':
                     for i in range(linha, linha + navio):
-                        mapa[i][coluna] = '🛳'
+                        mapa_computador[i][coluna] = '🛳'
                 else:
                     for j in range(coluna, coluna + navio):
-                        mapa[linha][j] = '🛳'
+                        mapa_computador[linha][j] = '🛳'
                 alocando_navio = True
-    return mapa
+    return mapa_computador
 
     # Verifica se acabou os 'N's da matriz
 def foi_derrotado(matriz):
@@ -336,5 +336,4 @@ def batalha_naval():
             jogar_novamente = input("Deseja jogar novamente? (s/n): ").lower()
 
     # EXECUÇÃO DO JOGO
-if __name__ == "__main__":
-    batalha_naval() 
+batalha_naval() 
